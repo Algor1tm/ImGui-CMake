37a7a9ac047e1bd8e895115b67e2672a2e9dd043
UserBug0 <lyiozel@gmail.com>
2022-07-15 14:01:40 +0300

    Add project files.

---
+++ ImGui-CMake.cpp
@@ -0,0 +1,12 @@
+﻿// ImGui-CMake.cpp : Defines the entry point for the application.
+//
+
+#include "ImGui-CMake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
